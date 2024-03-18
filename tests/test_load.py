c0222cd418710660bd043a6bcf3e7989a702aeb1
Stella <stellamaryane@gmail.com>
2024-03-18 13:49:53 -0300

    Adding fixtures, finalizers, setup and teardown to tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 
 from dundie.core import load
@@ -5,9 +7,20 @@ from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
+@pytest.fixture(scope="function", autouse=True)
+def create_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("new file content")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
 @pytest.mark.high
 def test_load():
     """Test load function."""
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
+        file_.write("arquivo indesejado")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == "J"
